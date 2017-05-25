33982e7c529b3926bf61a21171208af99bc5a2c5
SGitProjects <mjdigg18@gmail.com>
2017-05-25 19:22:18 -0400

    auto refresh

--- js/digi.js
+++ js/digi.js
@@ -1,6 +1,5 @@
-$(document).ready(function(){
-
 loadDigi();
+
    function loadDigi() {
         var url="https://api.coinmarketcap.com/v1/ticker/digibyte/";
         $.getJSON(url, function(data) {
@@ -12,23 +11,8 @@ loadDigi();
             $('#percertchangetwentyfour').html(data[0].percent_change_24h);
             $('#percertchangesevendays').html(data[0].percent_change_7d);    
         });
-
+            setInterval("loadDigi()", 10000);
    }
-
-     setInterval( function() {
-        var url="https://api.coinmarketcap.com/v1/ticker/digibyte/";
-        $.getJSON(url, function(data) {
-            $('#digipricebtc').html(data[0].price_btc);
-            $('#digipriceusd').html(data[0].price_usd);
-            $('#marketcap').html(data[0].market_cap_usd);
-            $('#supply').html(data[0].total_supply);   
-            $('#percertchangeone').html(data[0].percent_change_1h);  
-            $('#percertchangetwentyfour').html(data[0].percent_change_24h);
-            $('#percertchangesevendays').html(data[0].percent_change_7d);    
-        }, 5000);
-
-     });
-});
 
        function getTotalPriceUSD() {
             var usd1 = parseFloat(document.getElementById('digipriceusd1').value);
